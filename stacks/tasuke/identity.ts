4e6993c29858f7a7da5c4dd1acc20aab72d42948
Choko <choko@curioswitch.org>
2024-07-05 17:00:06 +0900

    Allow terraform-viewer to fetch firebase config secrets

--- stacks/tasuke/identity.ts
+++ stacks/tasuke/identity.ts
@@ -1,6 +1,8 @@
 import { DataGoogleKmsSecret } from "@cdktf/provider-google/lib/data-google-kms-secret";
 import { IdentityPlatformConfig } from "@cdktf/provider-google/lib/identity-platform-config";
 import { IdentityPlatformDefaultSupportedIdpConfig } from "@cdktf/provider-google/lib/identity-platform-default-supported-idp-config";
+import { ProjectIamCustomRole } from "@cdktf/provider-google/lib/project-iam-custom-role";
+import { ProjectIamMember } from "@cdktf/provider-google/lib/project-iam-member";
 import { ProjectService } from "@cdktf/provider-google/lib/project-service";
 import { Construct } from "constructs";
 
@@ -50,6 +52,27 @@ export class Identity extends Construct {
       idpId: "github.com",
       clientId: config.githubClientId,
       clientSecret: githubClientSecret.plaintext,
+    });
+
+    // terraform-viewer account has Viewer role which cannot fetch the client secret above,
+    // causing plans to be incorrect. It is decrypting the value itself from KMS so there is
+    // no additional visibliity if also giving it access to fetch the secret from firebase
+    // itself.
+    const secretViewerRole = new ProjectIamCustomRole(
+      this,
+      "firebaseauth-config-secret-viewer",
+      {
+        project: config.project,
+        title: "Firebase Auth Config Secret Viewer",
+        roleId: "firebaseauthConfigsSecretViewer",
+        permissions: ["firebaseauth.configs.getSecret"],
+      },
+    );
+
+    new ProjectIamMember(this, "terraform-viewer-firebase-secret", {
+      project: config.project,
+      role: secretViewerRole.name,
+      member: `serviceAccount:terraform-viewer@${config.project}.iam.gserviceaccount.com`,
     });
   }
 }
